cf30b79d4cc820ff8720e45aaae499ce1b2addd4
istrybuk <79363807+istrybuk@users.noreply.github.com>
2023-06-14 21:18:35 +0300

    added version 2 and initialized the driver

---
+++ A1/Folder_v2/tests/driver.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+import time
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.get()
+
+time.sleep(10)
