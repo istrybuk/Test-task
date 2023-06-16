e59c1c025e8df16fcec53f4b341d2b8f1e1b99e1
istrybuk <79363807+istrybuk@users.noreply.github.com>
2023-06-17 00:56:42 +0300

    update repository

--- A1/Folder_v2/tests/conftest.py
+++ A1/Folder_v2/tests/conftest.py
@@ -4,9 +4,8 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(autouse=True)
 def browser():
     service = Service(executable_path=ChromeDriverManager().install())
     driver = webdriver.Chrome(service=service)
     yield driver
-    driver.quit()
