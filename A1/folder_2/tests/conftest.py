7d9b1e9724e70c5e441995dd72f10d0659a09f1d
istrybuk <79363807+istrybuk@users.noreply.github.com>
2023-06-18 11:52:14 +0300

    add folder_2

---
+++ A1/folder_2/tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture(autouse=True)
+def browser():
+    service = Service(executable_path=ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service)
+    yield driver
